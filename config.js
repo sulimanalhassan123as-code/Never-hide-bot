2c3f0c9bf15a4ac3a40b33d381555d2df42c033b
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2026-02-12 21:01:10 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // ⚙️ SETTINGS
+    botName: "NeverHide V3",
+    ownerName: "Suleiman",
+    
+    // ⚠️ IMPORTANT: CHANGE THIS NUMBER TO SWITCH OWNERS
+    // format: 233XXXXXXXXX (No + sign)
+    ownerNumber: "233248503631", 
+    
+    // true = Pairing Code (Easy), false = QR Code (Scan)
+    usePairingCode: true,
+    
+    // The symbol before commands (e.g., !menu or .menu)
+    prefix: "!",
+    
+    // Auto-reply to everything? (Usually false)
+    autoRead: false
+};
