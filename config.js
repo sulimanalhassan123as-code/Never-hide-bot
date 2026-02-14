93034dad8b25a673098fe987c3eb087fb3920ab0
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2026-02-14 22:30:06 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    // ⚙️ SETTINGS
     botName: "NeverHide V3",
     ownerName: "Suleiman",
-    
-    // ✅ VERIFIED PHONE NUMBER
     ownerNumber: "233257671651", 
-    
-    // Command prefix (e.g., !ping)
     prefix: "!"
 };
